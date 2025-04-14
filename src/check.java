0282b84951909ca8efb0e8c74bcb2fcd4de491d9
katlehokhutsoane <st10442069@rcconnect.edu.za>
2025-04-14 10:45:26 +0200

    PROGRAMMING PART1 SEMESTER1

---
+++ src/check.java
@@ -0,0 +1,57 @@
+import javax.xml.namespace.QName;
+
+public class check extends Main {
+
+    void check() {
+
+        //username and password for testing
+        String testName="katleho_";
+        String testPassword="Pass_123";
+        String testPhone="0812345678";
+
+
+        boolean isValidUsername=checkUserName(testName);
+        boolean isValidPassword=checkPasswordComplexity(testPassword);
+        boolean isValidPhone=checkCellPhoneNumber(testPhone);
+        boolean isValidRegistered=registerUser(testName,testPassword);
+
+
+
+        if (isValidUsername) {//this if statement condition checks if the username is to according to its specifications
+            System.out.println("Username Successfully Captured");
+        } else {
+            System.out.println("Username is not correctly formatted ,please ensure that your username contains an " +
+                    "underscore and is no more than five characters in length.");
+
+        }
+
+        if (isValidPassword) {//checks if password is valid
+            System.out.println("Password Successfully Captured");
+        } else {
+            System.out.println("Password is not correctly formatted ,please ensure that your password contains at least " +
+                    "eight characters a capital letter,a number and a special character.");
+
+
+        }
+
+        if (isValidPhone) {//checks if phone number is valid
+            System.out.println("correct cellphone insertation");
+        } else {
+            System.out.println("TRY AGAIN invalid phone number.");
+
+        }
+
+
+
+        if (isValidRegistered) {//checks is it is a valid registration
+            System.out.println("you have been registered successfully");
+        } else {
+            System.out.println("incorrect TRY AGAIN.");
+
+        }
+            check check=new check();
+            check.check();
+
+        }
+
+    }
